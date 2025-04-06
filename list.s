e2ef2e3df5cda823c04d02f6ed3038873e45154e
iO <senpai9464@yandex.ru>
2025-04-06 18:41:42 +0300

    -

--- list.s
+++ list.s
@@ -7,19 +7,15 @@
 DATA type·main·Node(SB)/8, $0
 GLOBL type·main·Node(SB), RODATA, $8
 
-TEXT runtime·newobject(SB), NOSPLIT, $0-8
-  RET
-
 TEXT ·initList(SB), NOSPLIT, $8-16
-  MOVQ data+0(FP), AX          
-  MOVQ $type·main·Node(SB), CX 
-  MOVQ CX, 0(SP)               
-  CALL runtime·newobject(SB)   
-  MOVQ 8(SP), BX               
-  MOVQ AX, 0(BX)               
-  MOVQ $0, 8(BX)               
-  MOVQ $0, 16(BX)              
-  MOVQ BX, ret+8(FP)              
-  ADDQ $8, SP            
-  CALL fmt·Println(SB)    
-  RET      
+  MOVQ data+0(FP), DX
+  MOVQ $type·main·Node(SB), CX
+  MOVQ CX, 0(SP)
+  CALL runtime·newobject(SB)
+  MOVQ AX, BX
+  MOVQ DX, 0(BX)
+  MOVQ $0, 8(BX)
+  MOVQ $0, 16(BX)
+  MOVQ BX, ret+8(FP)
+  ADDQ $8, SP
+  RET
